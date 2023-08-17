bbe6618fd2d6886a9fb52bccb0289bf91e6833be
Danram <125492779+Oliverahdan@users.noreply.github.com>
2023-08-17 13:47:16 -0300

    Add files via upload
    tempo de atividade

--- main.dart
+++ main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 void main() {
   runApp(MyApp());
@@ -8,61 +9,89 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MyScreen(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Cronômetro de Tempo de Atividade'),
+        ),
+        body: TempoDeAtividade(),
+      ),
     );
   }
 }
 
-class MyScreen extends StatefulWidget {
+class TempoDeAtividade extends StatefulWidget {
   @override
-  _MyScreenState createState() => _MyScreenState();
+  _TempoDeAtividadeState createState() => _TempoDeAtividadeState();
 }
 
-class _MyScreenState extends State<MyScreen> {
-  String _selectedState = 'Amigável';
+class _TempoDeAtividadeState extends State<TempoDeAtividade> {
+  late Timer _timer;
+  int _segundosAtivos = 0;
 
-  Map<String, Color> _statesColors = {
-    'Amigável': Colors.orange,
-    'Em paz': Colors.white,
-    'Confiante': Colors.blue,
-    'Animado': Colors.yellow,
-    'Furioso': Colors.red,
-    'Sensivel': Colors.purple,
+  Color _backgroundColor = Colors.white;
+  String _mensagem = '';
 
-  };
+  @override
+  void initState() {
+    super.initState();
+    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
+      setState(() {
+        _segundosAtivos++;
+        if (_segundosAtivos == 5) {
+          _backgroundColor = Colors.yellow;
+        } else if (_segundosAtivos == 10) {
+          _backgroundColor = Colors.orange;
+        } else if (_segundosAtivos == 15) {
+          _backgroundColor = Colors.red;
+          _mensagem = 'Hello World';
+        } else if (_segundosAtivos == 20) {
+          _timer.cancel();
+          Navigator.pop(context);
+        }
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
+
+  String _formatDuration(Duration duration) {
+    String twoDigits(int n) {
+      if (n >= 10) return "$n";
+      return "0$n";
+    }
+
+    String twoDigitMinutes = twoDigits(duration.inMinutes.remainder(60));
+    String twoDigitSeconds = twoDigits(duration.inSeconds.remainder(60));
+
+    return "${twoDigits(duration.inHours)}:$twoDigitMinutes:$twoDigitSeconds";
+  }
 
   @override
   Widget build(BuildContext context) {
-    Color backgroundColor = _statesColors[_selectedState] ?? Colors.white;
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Estado Emocional'),
-      ),
-      backgroundColor: backgroundColor,
-      body: Center(
+    return Container(
+      color: _backgroundColor,
+      child: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
+          children: [
             Text(
-              'Selecione o estado mental de hoje:',
+              'Tempo de Atividade:',
               style: TextStyle(fontSize: 18),
             ),
-            SizedBox(height: 20),
-            DropdownButton<String>(
-              value: _selectedState,
-              onChanged: (newValue) {
-                setState(() {
-                  _selectedState = newValue!;
-                });
-              },
-              items: _statesColors.keys.map<DropdownMenuItem<String>>((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value),
-                );
-              }).toList(),
+            SizedBox(height: 10),
+            Text(
+              _formatDuration(Duration(seconds: _segundosAtivos)),
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
             ),
-            SizedBox(height: 20),
+            SizedBox(height: 10),
+            Text(
+              _mensagem,
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
           ],
         ),
       ),
