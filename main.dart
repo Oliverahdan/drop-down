0056c860336444311ea006bd38611308871b3595
Danram <125492779+Oliverahdan@users.noreply.github.com>
2023-08-17 17:29:34 -0300

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,91 +10,120 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Cronômetro de Tempo de Atividade'),
+        appBar: AppBar(title: Text('Imagem Clicável')),
+        body: Center(
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CartaoVisita(),
+                  SizedBox(height: 20),
+                  ImagensRedesSociais(),
+                ],
+              ),
+            ],
+          ),
         ),
-        body: TempoDeAtividade(),
       ),
     );
   }
 }
 
-class TempoDeAtividade extends StatefulWidget {
-  @override
-  _TempoDeAtividadeState createState() => _TempoDeAtividadeState();
-}
-
-class _TempoDeAtividadeState extends State<TempoDeAtividade> {
-  late Timer _timer;
-  int _segundosAtivos = 0;
-
-  Color _backgroundColor = Colors.white;
-  String _mensagem = '';
-
-  @override
-  void initState() {
-    super.initState();
-    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
-      setState(() {
-        _segundosAtivos++;
-        if (_segundosAtivos == 5) {
-          _backgroundColor = Colors.yellow;
-        } else if (_segundosAtivos == 10) {
-          _backgroundColor = Colors.orange;
-        } else if (_segundosAtivos == 15) {
-          _backgroundColor = Colors.red;
-          _mensagem = 'Hello World';
-        } else if (_segundosAtivos == 20) {
-          _timer.cancel();
-          Navigator.pop(context);
-        }
-      });
-    });
-  }
-
-  @override
-  void dispose() {
-    _timer.cancel();
-    super.dispose();
-  }
-
-  String _formatDuration(Duration duration) {
-    String twoDigits(int n) {
-      if (n >= 10) return "$n";
-      return "0$n";
-    }
-
-    String twoDigitMinutes = twoDigits(duration.inMinutes.remainder(60));
-    String twoDigitSeconds = twoDigits(duration.inSeconds.remainder(60));
-
-    return "${twoDigits(duration.inHours)}:$twoDigitMinutes:$twoDigitSeconds";
-  }
-
+class CartaoVisita extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: _backgroundColor,
-      child: Center(
+    return Card(
+      elevation: 5,
+      margin: EdgeInsets.all(20),
+      child: Padding(
+        padding: const EdgeInsets.all(20),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
-              'Tempo de Atividade:',
-              style: TextStyle(fontSize: 18),
-            ),
-            SizedBox(height: 10),
-            Text(
-              _formatDuration(Duration(seconds: _segundosAtivos)),
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 10),
-            Text(
-              _mensagem,
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
+            Text('Nome: Daniel'),
+            Text('Cargo: Técnico em Informática'),
+            Text('Número: 37 4002 8922'),
+            Text('Email: flavindopneu@gmail.com'),
+            Text('Manutenção e Suporte Técnico'),
           ],
         ),
       ),
+    );
+  }
+}
+
+class ImagensRedesSociais extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        InkWell(
+          onTap: () async {
+            const url = 'https://www.instagram.com/';
+            if (await canLaunch(url)) {
+              await launch(url);
+            } else {
+              throw 'Não foi possível abrir o link $url';
+            }
+          },
+          child: Image.asset(
+            'assets/instagram.png',
+            width: 70,
+            height: 70,
+          ),
+        ),
+        SizedBox(width: 20),
+        InkWell(
+          onTap: () async {
+            const url = 'https://pt-br.facebook.com/';
+            if (await canLaunch(url)) {
+              await launch(url);
+            } else {
+              throw 'Não foi possível abrir o link $url';
+            }
+          },
+          child: Image.asset(
+            'assets/facebook.png',
+            width: 70,
+            height: 70,
+          ),
+        ),
+        SizedBox(width: 20),
+        InkWell(
+          onTap: () async {
+            const url = 'https://twitter.com/';
+            if (await canLaunch(url)) {
+              await launch(url);
+            } else {
+              throw 'Não foi possível abrir o link $url';
+            }
+          },
+          child: Image.asset(
+            'assets/twitter.png',
+            width: 70,
+            height: 70,
+          ),
+        ),
+        SizedBox(width: 20),
+        InkWell(
+          onTap: () async {
+            const url = 'https://www.whatsapp.com/';
+            if (await canLaunch(url)) {
+              await launch(url);
+            } else {
+              throw 'Não foi possível abrir o link $url';
+            }
+          },
+          child: Image.asset(
+            'assets/whatsapp.png',
+            width: 70,
+            height: 70,
+          ),
+        ),
+      ],
     );
   }
 }
