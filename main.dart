ec317b2f77e1c907cdf6482e53ef341698fd85f2
Danram <125492779+Oliverahdan@users.noreply.github.com>
2023-08-18 13:48:46 -0300

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,43 +9,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text('Imagem Clicável')),
-        body: Center(
-          child: Stack(
-            alignment: Alignment.center,
-            children: [
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  CartaoVisita(),
-                  SizedBox(height: 20),
-                  ImagensRedesSociais(),
-                ],
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class CartaoVisita extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      elevation: 5,
-      margin: EdgeInsets.all(20),
-      child: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+        appBar: AppBar(title: Text('Cabeçalho Personalizado')),
+        body: Column(
           children: [
-            Text('Nome: Daniel'),
-            Text('Cargo: Técnico em Informática'),
-            Text('Número: 37 4002 8922'),
-            Text('Email: flavindopneu@gmail.com'),
-            Text('Manutenção e Suporte Técnico'),
+            CabecalhoPersonalizado(),
+            // Outros widgets podem ser adicionados aqui abaixo do cabeçalho
           ],
         ),
       ),
@@ -54,76 +21,30 @@ class CartaoVisita extends StatelessWidget {
   }
 }
 
-class ImagensRedesSociais extends StatelessWidget {
+class CabecalhoPersonalizado extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        InkWell(
-          onTap: () async {
-            const url = 'https://www.instagram.com/';
-            if (await canLaunch(url)) {
-              await launch(url);
-            } else {
-              throw 'Não foi possível abrir o link $url';
-            }
-          },
-          child: Image.asset(
-            'assets/instagram.png',
-            width: 70,
-            height: 70,
+    return Container(
+      color: Color(0xFFfcd167), // Cor de fundo #FCD167
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 40),
+      child: Row(
+        children: [
+          Icon(
+            Icons.person,
+            color: Colors.black,
+            size: 50,
           ),
-        ),
-        SizedBox(width: 20),
-        InkWell(
-          onTap: () async {
-            const url = 'https://pt-br.facebook.com/';
-            if (await canLaunch(url)) {
-              await launch(url);
-            } else {
-              throw 'Não foi possível abrir o link $url';
-            }
-          },
-          child: Image.asset(
-            'assets/facebook.png',
-            width: 70,
-            height: 70,
+          SizedBox(width: 20),
+          Text(
+            'Bem Vindo (a)',
+            style: TextStyle(
+              color: Color(0xFF765A13), // Cor do texto #765A13
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
           ),
-        ),
-        SizedBox(width: 20),
-        InkWell(
-          onTap: () async {
-            const url = 'https://twitter.com/';
-            if (await canLaunch(url)) {
-              await launch(url);
-            } else {
-              throw 'Não foi possível abrir o link $url';
-            }
-          },
-          child: Image.asset(
-            'assets/twitter.png',
-            width: 70,
-            height: 70,
-          ),
-        ),
-        SizedBox(width: 20),
-        InkWell(
-          onTap: () async {
-            const url = 'https://www.whatsapp.com/';
-            if (await canLaunch(url)) {
-              await launch(url);
-            } else {
-              throw 'Não foi possível abrir o link $url';
-            }
-          },
-          child: Image.asset(
-            'assets/whatsapp.png',
-            width: 70,
-            height: 70,
-          ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
